515b7d968d46a0a23b451ab426d5f5e43973b7af
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-08-27 10:14:53 +0900

    version up

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object BaseUnitsBuild extends Build {
     base = file("."),
     settings = Defaults.defaultSettings ++ Seq(
       organization := "org.sisioh",
-      version := "0.1.9",
+      version := "0.1.10",
       scalaVersion := "2.10.2",
       scalacOptions ++= Seq("-feature", "-unchecked", "-deprecation"),
       resolvers ++= Seq(
